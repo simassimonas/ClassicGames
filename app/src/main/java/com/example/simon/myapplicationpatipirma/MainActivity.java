28c64dc585e0b2b6104d1f0c1b767a77d466c093
simassimonas <simonassaltenis@gmail.com>
2018-11-19 10:22:00 +0200

    Initial commit

---
+++ app/src/main/java/com/example/simon/myapplicationpatipirma/MainActivity.java
@@ -0,0 +1,37 @@
+package com.example.simon.myapplicationpatipirma;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button btnToSignUp;
+    private Button btnToChooseGame;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnToSignUp = (Button)findViewById(R.id.btnSignup);
+        btnToSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(MainActivity.this,SignUpActivity.class);
+                startActivity(i);
+            }
+        });
+
+        btnToChooseGame = (Button)findViewById(R.id.btnLogin);
+        btnToChooseGame.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(MainActivity.this,ChooseGameActivity.class);
+                startActivity(i);
+            }
+        });
+
+    }
+}
